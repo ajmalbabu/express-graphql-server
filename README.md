2edd4df5c5bcd4f54cd3a9e8970443ea6f3fef9a
Admin <admin@Admins-MacBook-Pro.local>
2020-06-04 07:42:11 -0500

    Created a graphql server

---
+++ README.md
@@ -0,0 +1,44 @@
+## This project sets up a graphql server that connects to spacex data over REST API and exposes the data as a set of 
+## Graphql end points. This project is created by watching [this](https://www.youtube.com/watch?v=SEMTj8w04Z8) video 
+## Space X [API](https://docs.spacexdata.com/?version=latest#bc65ba60-decf-4289-bb04-4ca9df01b9c1)
+
+## To run server
+
+npm run server
+
+and hit graphiql end point at http://localhost:5000/
+
+use below GraphQL queries to reteive data
+
+{
+     launches {
+        flight_number,
+        mission_name,
+        launch_year,
+        launch_success,
+        launch_date_local,
+        rocket {
+          rocket_id,
+          rocket_name,
+          rocket_type
+        }
+      }
+    }
+
+
+  {
+      launch (flight_number: 2) {
+        mission_name,
+        launch_year
+      }
+    }
+
+
+  {
+  	rockets {
+      rocket_name
+    }
+  }
+
+## Watch 5 part series on creating graphql server CRUD server with mutation [here](https://www.youtube.com/watch?v=PEcJxkylcRM)
+
---
+++ schema.js
@@ -0,0 +1,112 @@
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema} = require('graphql');
+const axios = require('axios');
+
+
+// Launch type
+
+const LaunchType = new GraphQLObjectType({
+
+    name: 'Launch',
+    fields: () => ({
+        flight_number: {type: GraphQLInt},
+        mission_name: {type: GraphQLString},
+        launch_year: {type: GraphQLString},
+        launch_date_local: {type: GraphQLString},
+        launch_success: {type: GraphQLBoolean},
+        rocket: {type: RocketType}
+    })
+});
+
+// Rocket type
+
+const RocketType = new GraphQLObjectType({
+
+    name: 'Rocket',
+    fields: () => ({
+        rocket_id: {type: GraphQLString},
+        rocket_name: {type: GraphQLString},
+        rocket_type: {type: GraphQLString}
+    })
+});
+
+// Root query
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => ({
+        launches: {
+            type: new GraphQLList(LaunchType),
+            resolve(parent,args) {
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: {type: GraphQLInt}
+            },
+            resolve(parent,args) {
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                    .then(res => res.data);
+            }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent,args) {
+                return axios.get('https://api.spacexdata.com/v3/rockets')
+                    .then(res => res.data);
+            }
+        },
+        rocket: {
+            type: RocketType,
+            args: {
+                rocket_id: {type: GraphQLInt}
+            },
+            resolve(parent,args) {
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+                    .then(res => res.data);
+            }
+        }
+
+    })
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
+
+// 5 part series on creating graphql server CRUD server with mutation: https://www.youtube.com/watch?v=PEcJxkylcRM
+// https://docs.spacexdata.com/?version=latest#bc65ba60-decf-4289-bb04-4ca9df01b9c1
+// http://localhost:5000/graphql
+
+// {
+//     launches {
+//       flight_number,
+//       mission_name,
+//       launch_year,
+//       launch_success,
+//       launch_date_local,
+//       rocket {
+//         rocket_id,
+//         rocket_name,
+//         rocket_type
+//       }
+//     }
+//   }
+
+
+// {
+//     launch (flight_number: 2) {
+//       mission_name,
+//       launch_year
+//     }
+//   }
+
+
+// {
+// 	rockets {
+//     rocket_name
+//   }
+// }
